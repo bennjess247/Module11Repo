cf68dc9a95aa8c6deffee763c8f295b4f992da0c
bennjess247 <bennjess247@gmail.com>
2025-12-02 13:30:34 -0500

    Update hello.py
    Added an excuse

--- hello.py
+++ hello.py
@@ -6,6 +6,12 @@
 # 12/01/2025
 # Added New Message
 
+# Benjamin Kilgore
+# 12/02/2025
+# Added An Excuse
+
 print("Hello, World!")
 
 print("Welcome to GitHub!")
+
+print("The notification for the next step must have expired, thus this message. Hopefully it works!")
